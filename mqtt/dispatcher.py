3f25c2267b6cfeaf72031245602456f77d77ac72
valtteriah <valtteri.ahonen@student.oulu.fi>
2021-03-14 12:07:08 +0200

    mqtt server ja dispatcher päivitetty

--- mqtt/dispatcher.py
+++ mqtt/dispatcher.py
@@ -1,21 +1,19 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
+import json
 
 def on_message(client, userdata, message):
-    clients = str(message.payload.decode("utf-8"))
+    message = str(message.payload.decode("utf-8"))
+    dictionary = json.loads(message)
+    clients = dictionary["clients"]
+    client_id = dictionary["client_id"]
     print("Number of clients" ,clients)
+    print("Client id", client_id)
 
 broker_address="127.0.0.1"
 
 client = mqtt.Client("dispatcher") #create new instance
 client.on_message=on_message
 client.connect(broker_address) #connect to broker
-client.loop_start()
 client.subscribe("clients")
-
-
-while True:
-    client.publish("call_clients", "CALL")
-    time.sleep(10)
-
-client.loop_stop()
+client.loop_forever()
--- mqtt/server.py
+++ mqtt/server.py
@@ -1,18 +1,32 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
+import json
 from random import randrange, uniform
 
+"""
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
 
     clients = uniform(0, 100)
     client.publish("clients", clients)
+"""
 
 broker_address="127.0.0.1"
 
 client = mqtt.Client("server") #create new instance
-client.on_message=on_message #attach function to callback
 client.connect(broker_address) #connect to broker
+client.loop_start()
 
-client.subscribe("call_clients")
-client.loop_forever()
+while True:
+    client_id = "server"
+    clients = uniform(0, 100)
+    dictionary = {
+        "client_id": client_id,
+        "clients": clients
+    }
+    client_number = json.dumps(dictionary)
+    client.publish("clients", client_number)
+    print("Message published", client_number)
+    time.sleep(10)
+
+client.loop_stop()
